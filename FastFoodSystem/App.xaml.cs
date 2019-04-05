c4bc09df682019d25fa303f182dbf0709159b226
Bernardo Caussin <lbct95@gmail.com>
2019-04-05 02:14:37 -0400

    Test

---
+++ FastFoodSystem/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using FastFoodSystem.Database;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace FastFoodSystem
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static DatabaseEntities databaseEntities { get; private set; }
+
+        public App()
+        {
+           this.InitializeComponent();
+            databaseEntities = new DatabaseEntities();
+            MessageBox.Show(databaseEntities.Database.Connection.State.ToString());
+        }
+    }
+}
---
+++ FastFoodSystem/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FastFoodSystem
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        public async void RefreshTable()
+        {
+            var tableData = await Task.Factory.StartNew(() =>
+            {
+                var data = (from t in App.databaseEntities.Tables select t).ToArray();
+                return data;
+            });
+            table.ItemsSource = tableData;
+        }
+
+        private async void Add_item_button_Click(object sender, RoutedEventArgs e)
+        {
+            var input = new InputBox();
+            input.ShowDialog();
+            string name = input.text.Text;
+            input = new InputBox();
+            input.ShowDialog();
+            string type = input.text.Text;
+            await Task.Factory.StartNew(() => 
+            {
+                Database.Table table = new Database.Table()
+                {
+                    Name = name,
+                    Type = type
+                };
+                App.databaseEntities.Tables.Add(table);
+                App.databaseEntities.SaveChanges();
+            });
+            MessageBox.Show("Saved "+App.databaseEntities.Database.Connection.State);
+        }
+
+        private void Refresh_button_Click(object sender, RoutedEventArgs e)
+        {
+            RefreshTable();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+        }
+    }
+}
