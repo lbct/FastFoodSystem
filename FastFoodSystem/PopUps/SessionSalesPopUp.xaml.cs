9a34cd712d5c88df8e9429ffd17ca9cd2a598ab6
Bernardo <lbct95@gmail.com>
2023-07-17 22:01:56 -0400

    fix

--- FastFoodSystem/PopUps/SessionSalesPopUp.xaml.cs
+++ FastFoodSystem/PopUps/SessionSalesPopUp.xaml.cs
@@ -29,25 +29,28 @@ namespace FastFoodSystem.PopUps
 
         public async Task Init()
         {
-            sales_table.ItemsSource = await App.RunAsync(() => 
+            var itemsSource = await App.RunAsync(() =>
             {
                 var sales = App.Database.Sales
                 .Where(s => !s.Hide && s.LoginId == UserSession.LoginID)
                 .OrderByDescending(s => s.DailyId)
                 .ToArray();
                 var items = new List<SessionSaleItem>();
-                foreach(var sale in sales)
+                foreach (var sale in sales)
                 {
                     var client = App.Database.Clients.FirstOrDefault(c => c.Id == sale.ClientId);
                     var item = new SessionSaleItem()
                     {
                         Sale = sale,
-                        Client = client
+                        Client = client,
+                        TotalValue = sale.SaleDetails.Sum(sd => sd.Units * sd.UnitValue)
                     };
                     items.Add(item);
                 }
                 return items;
             });
+            sales_table.ItemsSource = itemsSource;
+            total_sale_value.Value = double.Parse(itemsSource.Sum(i => i.TotalValue).ToString());
         }
 
         private void View_sale_button_Initialized(object sender, EventArgs e)
@@ -73,5 +76,6 @@ namespace FastFoodSystem.PopUps
     {
         public Sale Sale { get; set; }
         public Client Client { get; set; }
+        public decimal TotalValue { get; set; }
     }
 }
